32e0c6ccde4502bfeafd8a78c614f6ffa781d3b5
Patryk Woziński <patryk.wozinski@gmail.com>
2019-05-10 13:24:29 +0200

    qfix

--- src/Kernel.php
+++ src/Kernel.php
@@ -84,9 +84,4 @@ class Kernel extends BaseKernel
 		$routes->import($confDir . '/{routes}/*' . self::CONFIG_EXTS, '/', 'glob');
 		$routes->import($confDir . '/{routes}' . self::CONFIG_EXTS, '/', 'glob');
 	}
-
-	protected function build(ContainerBuilder $container): void
-	{
-		$container->addCompilerPass(new MessageBusCompilerPass);
-	}
 }
