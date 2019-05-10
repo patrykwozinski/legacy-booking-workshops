b2f7d6e7ce1ed571add5f24b4f88b62a951bd6c8
Patryk Woziński <patryk.wozinski@gmail.com>
2019-05-10 13:23:57 +0200

    kernel

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,7 +1,9 @@
 <?php
+declare(strict_types=1);
 
 namespace App;
 
+use App\Shared\Infrastructure\DependencyInjection\MessageBusCompilerPass;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\Config\Resource\FileResource;
@@ -11,43 +13,80 @@ use Symfony\Component\Routing\RouteCollectionBuilder;
 
 class Kernel extends BaseKernel
 {
-    use MicroKernelTrait;
+	use MicroKernelTrait;
 
-    private const CONFIG_EXTS = '.{php,xml,yaml,yml}';
+	private const CONFIG_EXTS = '.{php,xml,yaml,yml}';
 
-    public function registerBundles(): iterable
-    {
-        $contents = require $this->getProjectDir().'/config/bundles.php';
-        foreach ($contents as $class => $envs) {
-            if ($envs[$this->environment] ?? $envs['all'] ?? false) {
-                yield new $class();
-            }
-        }
-    }
+	private $appId;
 
-    public function getProjectDir(): string
-    {
-        return \dirname(__DIR__);
-    }
+	public function __construct(string $environment, bool $debug, string $appId = null)
+	{
+		$this->appId = $appId ?? \getenv('APP');
 
-    protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
-    {
-        $container->addResource(new FileResource($this->getProjectDir().'/config/bundles.php'));
-        $container->setParameter('container.dumper.inline_class_loader', true);
-        $confDir = $this->getProjectDir().'/config';
+		if (empty($this->appId))
+		{
+			throw new \InvalidArgumentException('APP_ID is empty!');
+		}
 
-        $loader->load($confDir.'/{packages}/*'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{packages}/'.$this->environment.'/**/*'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{services}'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{services}_'.$this->environment.self::CONFIG_EXTS, 'glob');
-    }
+		parent::__construct($environment, $debug);
+	}
 
-    protected function configureRoutes(RouteCollectionBuilder $routes): void
-    {
-        $confDir = $this->getProjectDir().'/config';
+	public function registerBundles(): iterable
+	{
+		$contents = require $this->getProjectDir() . '/config/bundles.php';
+		foreach ($contents as $class => $envs)
+		{
+			if ($envs[$this->environment] ?? $envs['all'] ?? false)
+			{
+				yield new $class;
+			}
+		}
+	}
 
-        $routes->import($confDir.'/{routes}/'.$this->environment.'/**/*'.self::CONFIG_EXTS, '/', 'glob');
-        $routes->import($confDir.'/{routes}/*'.self::CONFIG_EXTS, '/', 'glob');
-        $routes->import($confDir.'/{routes}'.self::CONFIG_EXTS, '/', 'glob');
-    }
+	public function getLogDir(): string
+	{
+		return \sprintf(
+			'%s/var/logs/%s/%s',
+			$this->getProjectDir(),
+			$this->environment,
+			$this->appId
+		);
+	}
+
+	public function getCacheDir(): string
+	{
+		return \sprintf(
+			'%s/var/cache/%s/%s',
+			$this->getProjectDir(),
+			$this->environment,
+			$this->appId
+		);
+	}
+
+	protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
+	{
+		$container->addResource(new FileResource($this->getProjectDir() . '/config/bundles.php'));
+		$container->setParameter('container.dumper.inline_class_loader', true);
+		$confDir = $this->getProjectDir() . '/config';
+
+		$loader->load($confDir . '/{packages}/*' . self::CONFIG_EXTS, 'glob');
+		$loader->load($confDir . '/{packages}/' . $this->environment . '/**/*' . self::CONFIG_EXTS, 'glob');
+		$loader->load($confDir . '/{services}' . self::CONFIG_EXTS, 'glob');
+		$loader->load($confDir . '/{services}_' . $this->environment . self::CONFIG_EXTS, 'glob');
+		$loader->load(\sprintf($confDir . '/app/%s.yml', $this->appId));
+	}
+
+	protected function configureRoutes(RouteCollectionBuilder $routes): void
+	{
+		$confDir = $this->getProjectDir() . '/config';
+
+		$routes->import($confDir . '/{routes}/' . $this->environment . '/**/*' . self::CONFIG_EXTS, '/', 'glob');
+		$routes->import($confDir . '/{routes}/*' . self::CONFIG_EXTS, '/', 'glob');
+		$routes->import($confDir . '/{routes}' . self::CONFIG_EXTS, '/', 'glob');
+	}
+
+	protected function build(ContainerBuilder $container): void
+	{
+		$container->addCompilerPass(new MessageBusCompilerPass);
+	}
 }
