3452d05bef9f80b1bb83f32a2eae1b8c20062218
Patryk Woziński <patryk.wozinski@gmail.com>
2019-05-11 15:30:06 +0200

    start

---
+++ src/Controller/BookingController.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class BookingController extends AbstractController
+{
+	public function bookVisit()
+	{
+
+	}
+}
---
+++ src/Service/BookingHelper.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+class BookingHelper
+{
+	public function create(string $date, int $doctorId, ?string $patient)
+	{
+
+	}
+}
