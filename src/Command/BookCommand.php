b6d958eb77ff69b4c96d0af96a63a665553fd5ca
Patryk Woziński <patryk.wozinski@gmail.com>
2019-05-13 08:54:29 +0200

    cli

--- src/Command/BookCommand.php
+++ src/Command/BookCommand.php
@@ -4,12 +4,32 @@ declare(strict_types=1);
 namespace App\Command;
 
 
+use App\SDK\AvailabilityApiClient\AvailabilityApiClient;
+use App\SDK\AvailabilityApiClient\IO\Doctor;
+use App\Service\BookingHelper;
+use App\Service\BookingValidator;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class BookCommand extends Command
 {
+	private $bookingHelper;
+	private $availabilityApiClient;
+	private $bookingValidator;
+
+	public function __construct(
+		BookingHelper $bookingHelper,
+		AvailabilityApiClient $availabilityApiClient,
+		BookingValidator $bookingValidator
+	) {
+		parent::__construct();
+
+		$this->bookingHelper         = $bookingHelper;
+		$this->availabilityApiClient = $availabilityApiClient;
+		$this->bookingValidator      = $bookingValidator;
+	}
+
 	public function configure()
 	{
 		$this->setName('booking:book')
@@ -20,8 +40,32 @@ class BookCommand extends Command
 
 	public function execute(InputInterface $input, OutputInterface $output)
 	{
-		$doctorId = $input->getOption('doctorId');
+		$doctorId = (int)$input->getOption('doctorId');
 		$date     = $input->getOption('date');
 		$patient  = $input->getOption('patient');
+
+		$booking      = $this->bookingHelper->create($date, $doctorId, $patient);
+		$availability = $this->availabilityApiClient->getAvailabilityInformation(
+			new Doctor($doctorId),
+			new \DateTimeImmutable($date)
+		);
+
+		if (false === $availability->exists() || $availability->reserved())
+		{
+			$output->writeln('Given date does not exists in calendar or is reserved');
+
+			return 1;
+		}
+
+		$bookingStatus = $this->bookingValidator->checkIfValid($booking);
+
+		if ($bookingStatus)
+		{
+			$output->writeln('Booked!');
+
+			return 0;
+		}
+
+		$output->writeln('Cannot book with errors: ' . $bookingStatus);
 	}
 }
