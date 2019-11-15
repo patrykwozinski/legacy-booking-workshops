c7642ef5fde966b063ddee0f489911776efa917d
Michał Dobaczewski <mdobak@gmail.com>
2019-11-15 14:17:13 +0100

    Update DB schema on install

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -12,12 +12,12 @@ use App\Service\BookingHelper;
 use App\Service\BookingValidator;
 use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class BookingController extends AbstractController
 {
-    public function bookVisit(Request $request): JsonResponse
+    public function bookVisit(Request $request): Response
     {
         $date = $request->get('date');
         $doctorId = $request->get('doctorId');
@@ -25,7 +25,7 @@ class BookingController extends AbstractController
         /** @var EntityManager $em */
         $em = $this->get('doctrine');
         /** @var DoctorEntity $doctor */
-        $doctor = $em->getRepository(DoctorEntity::class)->find($doctorId);
+        $doctor = $em->getRepository(DoctorEntity::class)->find($doctorId ?? '');
 
         /** @var BookingHelper $bookingHelper */
         $bookingHelper = $this->get(BookingHelper::class);
@@ -39,7 +39,7 @@ class BookingController extends AbstractController
         );
 
         if (false === $availability->exists() || $availability->reserved()) {
-            return new JsonResponse('Given date does not exists in calendar or is reserved');
+            return new Response('Given date does not exists in calendar or is reserved');
         }
 
         /** @var BookingValidator $validator */
@@ -54,31 +54,17 @@ class BookingController extends AbstractController
             $em->persist($booking);
             $em->flush();
 
-            return new JsonResponse('Booked!');
+			$event = new BookedEvent();
+			$event->date = $date;
+			$event->doctorId = $doctorId;
+
+			/** @var EventDispatcherInterface $dispatcher */
+			$dispatcher = $this->get(EventDispatcherInterface::class);
+			$dispatcher->dispatch($event);
+
+            return new Response('Booked!');
         }
 
-        return new JsonResponse('Cannot book visit with errors: ' . $bookingStatus);
-    }
-
-    public function getBookings(Request $request): JsonResponse
-    {
-        /** @var EntityManager $em */
-        $em = $this->get('doctrine');
-
-        $bookings = $em->getRepository(Booking::class)->findBy([
-            'doctor_id' => $request->get('doctor_id'),
-        ]);
-
-        $bookings = array_map(function (Booking $booking) {
-            return [
-                'date' => $booking->getDate()->format('Y-m-d H:i:s'),
-                'patient' => $booking->getPatient(),
-            ];
-        }, $bookings);
-
-        return new JsonResponse([
-            'doctor' => $request->get('doctor_id'),
-            'bookings' => $bookings,
-        ]);
+        return new Response('Cannot book visit with errors: ' . $bookingStatus);
     }
 }
